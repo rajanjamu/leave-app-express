172bbab1c531b5e9ad987e52c77f0040d1fee14b
rajanjamu <rajanjamu@gmail.com>
2020-04-11 00:37:48 +0530

    user model working

--- app.js
+++ app.js
@@ -17,10 +17,35 @@ const indexRoutes   = require('./routes/index'),
       leaveRoutes   = require('./routes/leave');
 
 // App Config
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/leave_app', {
+    useNewUrlParser: true,
+    useFindAndModify: false,
+    useUnifiedTopology: true
+});
 app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static('public'));
+app.use(methodOverride('_method'));
+app.use(flash());
 
+// Passport Configurations
+app.use(require('express-session')({
+    secret: 'secret',
+    resave: false,
+    saveUninitialized: false
+}));
+app.use(passport.initialize());
+app.use(passport.session());
+passport.use(new LocalStrategy(User.authenticate()));
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
+
+app.use((req, res, next) => {
+    res.locals.currentUser = req.user;
+    res.locals.error = req.flash('error');
+    res.locals.success = req.flash('success');
+    next();
+});
 
 app.use(indexRoutes);
 app.use(leaveRoutes);
--- routes/index.js
+++ routes/index.js
@@ -1,19 +1,61 @@
 const express   = require('express'),
-      router    = express.Router();
+      router    = express.Router(),
+      passport  = require('passport'),
+      User      = require('../models/user');
 
 // ROOT
 router.get('/', (req, res) => {
-    res.render('leaves/index');
-});
-
-// LOGIN FORM
-router.get('/login', (req, res) => {
-    res.render('login');
+    res.redirect('/leaves');
 });
 
 // REGISTER FORM
 router.get('/register', (req, res) => {
+    if (req.user) {
+        req.flash('error', 'Already logged in!');
+        return res.redirect('/leaves');
+    }
     res.render('register');
+});
+
+// LOGIN FORM
+router.get('/login', (req, res) => {
+    if (req.user) {
+        req.flash('error', 'Already logged in!');
+        return res.redirect('/leaves');
+    }
+    res.render('login');
+});
+
+router.get('/logout', (req, res) => {
+    req.logout();
+    req.flash('success', 'Logged out successfully!')
+    res.redirect('/login');
+});
+
+router.post('/register', (req, res) => {
+    console.log(req.body);
+    User.register(new User({username: req.body.username}), req.body.password, (err, user) => {
+        if (err) {
+            req.flash('error', err.message);
+            return res.redirect('/register');
+        }
+        passport.authenticate('local')(req, res, () => {
+            req.flash('success', 'Welcome, ' + user.username);
+            res.redirect('/leaves');
+        });
+    });
+});
+
+router.post('/login', passport.authenticate('local', {
+    successRedirect: '/leaves',
+    failureRedirect: '/login',
+    failureFlash: 'Invalid username or password!',
+    successFlash: 'You are logged in!'
+}), (req, res) => {
+    passport.authenticate('local')(req, res, () => {
+        req.flash('success', 'You are logged in!');
+        res.redirect('/leaves');
+    });
 });
 
 module.exports = router;
