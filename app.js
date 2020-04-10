e5bf05a269647a0177f752faf39a45b7a2eebbf0
rajanjamu <rajanjamu@gmail.com>
2020-04-10 16:47:59 +0530

    partials and other views

--- app.js
+++ app.js
@@ -10,4 +10,12 @@ app.get('/', (req, res) => {
     res.render('index');
 });
 
+app.get('/login', (req, res) => {
+    res.render('login');
+});
+
+app.get('/register', (req, res) => {
+    res.render('register');
+});
+
 app.listen(process.env.PORT || 3000, console.log('Server running on 3000!'));
