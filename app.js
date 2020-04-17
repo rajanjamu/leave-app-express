7e864577fcd37831b9b6fe1aef8e8127e34d66b1
rajanjamu <rajanjamu@gmail.com>
2020-04-17 15:16:24 +0530

    seeding admin user

--- app.js
+++ app.js
@@ -28,7 +28,7 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static('public'));
 app.use(methodOverride('_method'));
 app.use(flash());
-//seedDB();
+seedDB();
 
 // Passport Configurations
 app.use(require('express-session')({
